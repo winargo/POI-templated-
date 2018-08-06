e73e844031c4f7d8fc68e811a85b19c76d5848c2
Riandy <rwinargo97@gmail.com>
2018-08-06 17:16:42 +0700

    aaa

--- app/src/main/java/com/material/components/activity_login.java
+++ app/src/main/java/com/material/components/activity_login.java
@@ -1,59 +1,212 @@
 package com.material.components;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.InstrumentationInfo;
+import android.os.AsyncTask;
 import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import com.google.gson.JsonObject;
 import com.material.components.utils.Tools;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class activity_login extends AppCompatActivity {
 
     private ProgressBar progress_bar;
     private FloatingActionButton fab;
+    SharedPreferences pref;
     private View parent_view;
+    TextView usr;
+    TextView pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_login);
-        parent_view = findViewById(android.R.id.content);
-        progress_bar = (ProgressBar) findViewById(R.id.progress_bar);
-        fab = (FloatingActionButton) findViewById(R.id.fab);
 
-        Tools.setSystemBarColor(this, R.color.cyan_800);
+        pref = getSharedPreferences("poipayroll",MODE_PRIVATE);
 
-        ((View) findViewById(R.id.sign_up_for_account)).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(parent_view, "Sign up for an account", Snackbar.LENGTH_SHORT).show();
-            }
-        });
+        if(!pref.getString("username","").equals("")){
+            Intent mainmenu = new Intent(activity_login.this,activity_mainmenu.class);
+            startActivity(mainmenu);
+        }
+        else {
 
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(final View v) {
-                searchAction();
-            }
-        });
+            usr = findViewById(R.id.usrname);
+            pass = findViewById(R.id.usrpass);
 
+            parent_view = findViewById(android.R.id.content);
+            progress_bar = (ProgressBar) findViewById(R.id.progress_bar);
+            fab = (FloatingActionButton) findViewById(R.id.fab);
+
+            Tools.setSystemBarColor(this, R.color.cyan_800);
+
+            ((View) findViewById(R.id.sign_up_for_account)).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Snackbar.make(parent_view, "Sign up for an account", Snackbar.LENGTH_SHORT).show();
+                }
+            });
+
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(final View v) {
+                    if(usr!=null && pass!=null){
+                        if(usr.getText().toString().equals("")){
+                            Snackbar.make(parent_view,"Username Is Empty",Snackbar.LENGTH_SHORT).show();
+
+                        }
+                        else if(pass.getText().toString().equals("")){
+                            Snackbar.make(parent_view,"Please Input Password",Snackbar.LENGTH_SHORT).show();
+                        }
+                        else {
+                            searchAction(usr.getText().toString(),pass.getText().toString());
+                        }
+                    }
+                }
+            });
+
+        }
     }
 
-    private void searchAction() {
+    private void searchAction(final String username, final String password) {
         progress_bar.setVisibility(View.VISIBLE);
         fab.setAlpha(0f);
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                progress_bar.setVisibility(View.GONE);
-                fab.setAlpha(1f);
-                Snackbar.make(parent_view, "Login data submitted", Snackbar.LENGTH_SHORT).show();
+                TestAsync sync = new TestAsync(activity_login.this,"http://"+generator.Server+"/poihrd/auth/login_ajax",username,password);
+                sync.execute();
+
+
             }
         }, 1000);
+    }
+
+    private class TestAsync extends AsyncTask<Void, Integer, String>
+    {
+        String response = "";
+        JSONObject object = null;
+        String username=  "" ;
+        String password = "" ;
+        ProgressDialog dialog ;
+        String urldata ;
+
+        public  TestAsync (Context context,String url,String username , String password)
+        {
+            dialog = new ProgressDialog(context);
+            this.urldata = url;
+            this.username = username;
+            this.password = password;
+        }
+
+        String TAG = getClass().getSimpleName();
+
+        protected void onPreExecute (){
+            super.onPreExecute();
+            Log.d(TAG + " PreExceute","On pre Exceute......");
+        }
+
+        protected String doInBackground(Void...arg0) {
+            Log.d(TAG + " DoINBackGround","On doInBackground...");
+
+            try {
+                response = okhttpclass.getJsonlogin(urldata,username,password).toString();
+                object = okhttpclass.getJsonlogin(urldata,username,password);
+            } catch (IOException e) {
+                object = null;
+                response = "Error IOException";
+            } catch (NullPointerException e){
+                object = null;
+                response = "Error Data Null Occured";
+            }catch (Exception e){
+                object = null;
+                response = "Error Occured, PLease Contact Administrator/Support";
+            }
+
+            return response;
+        }
+
+        protected void onProgressUpdate(Integer...a){
+            super.onProgressUpdate(a);
+            Log.d(TAG + " onProgressUpdate", "You are in progress update ... " + a[0]);
+        }
+
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            try {
+                if(object!=null){
+                    JSONObject responseObject = new JSONObject(response);
+                    String status= responseObject.getString("status");
+
+                    if(status.equals("true")){
+                        Snackbar.make(parent_view, "Success , Loging in ...", Snackbar.LENGTH_SHORT).show();
+
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+
+                                Intent mainmenu = new Intent(activity_login.this,activity_mainmenu.class);
+                                startActivity(mainmenu);
+
+                                SharedPreferences.Editor prefedit = pref.edit();
+                                prefedit.putString("username",username);
+                                prefedit.putString("password",password);
+                                prefedit.apply();
+
+                                finish();
+
+                            }
+                        }, 1000);
+
+                    }
+                    else if(status.equals("false")) {
+                        Snackbar.make(parent_view, status, Snackbar.LENGTH_SHORT).show();
+                    }
+                    else {
+                        Snackbar.make(parent_view, status, Snackbar.LENGTH_SHORT).show();
+                    }
+
+                    progress_bar.setVisibility(View.GONE);
+                    fab.setAlpha(1f);
+                }
+                else {
+                    Snackbar.make(parent_view, response, Snackbar.LENGTH_SHORT).show();
+                    progress_bar.setVisibility(View.GONE);
+                    fab.setAlpha(1f);
+                }
+
+
+
+                //JSONArray bArray= responseObject.getJSONArray("B");
+                //for(int i=0;i<bArray.length();i++){
+                //    JSONObject innerObject=bArray.getJSONObject(i);
+                //    String a= innerObject.getString("a");
+                //    String b= innerObject.getString("b");
+                //}
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+            Log.d(TAG + " onPostExecute", "" + result);
+        }
     }
 }
---
+++ app/src/main/java/com/material/components/okhttpclass.java
@@ -0,0 +1,100 @@
+package com.material.components;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.Map;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class okhttpclass {
+    OkHttpClient client = new OkHttpClient();
+
+    String run(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
+
+    public static String getString(String url) throws IOException {
+        okhttpclass example = new okhttpclass();
+        String response = example.run(url);
+        return response;
+    }
+
+    public static JSONArray getJsonArray(String url, Map<String, String> args) throws IOException {
+        try {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(url)
+                    .build();
+            Response responses = null;
+
+            try {
+                responses = client.newCall(request).execute();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            String jsonData = responses.body().string();
+
+            JSONArray jsonArray = new JSONArray(jsonData);
+
+            return jsonArray;
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static JSONObject getJsonlogin(String url,String user , String Pass) throws IOException {
+        try {
+            OkHttpClient client = new OkHttpClient();
+
+            JSONArray array = new JSONArray();
+
+            RequestBody body = new FormBody.Builder()
+                    .add("username", user)
+                    .add("password", Pass)
+                    .build();
+
+            Request request = new Request.Builder()
+                    .url(url)
+                    .post(body)
+                    .build();
+            Response responses = null;
+
+            try {
+                responses = client.newCall(request).execute();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+
+            if (responses==null){
+                return null;
+            }
+            else {
+                JSONObject jsonObject = new JSONObject(responses.body().string());
+                return jsonObject;
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
